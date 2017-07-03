dd115766b33ea0a9b092d4a5c757f72d1203f072
Matheus Tessaro <matbrnsred@gmail.com>
2017-07-03 17:59:32 -0300

    Criação das models de Categoria/Livro. Início das implementações do CRUD de Categorias.

---
+++ BibliotecaMVC/BibliotecaMVC/Models/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BibliotecaMVC.Models
+{
+    public class Categoria
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O preenchimento do campo é obrigatório, favor verificar.")]
+        [StringLength(100, ErrorMessage = "Máximo de 100 caractéres, favor verificar.")]
+        public string Nome { get; set; }
+
+        [Required]
+        public bool Status { get; set; }
+    }
+}
---
+++ BibliotecaMVC/BibliotecaMVC/Models/Livro.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BibliotecaMVC.Models
+{
+    public class Livro
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O preenchimento do campo é obrigatório, favor verificar.")]
+        [StringLength(100, ErrorMessage = "Máximo de 100 caractéres, favor verificar.")]
+        public string Nome { get; set; }
+
+        [StringLength(500, ErrorMessage = "Máximo de 500 caractéres, favor verificar.")]
+        public string Sinopse { get; set; }
+
+        [Required(ErrorMessage = "O preenchimento do campo é obrigatório, favor verificar.")]
+        [StringLength(100, ErrorMessage = "Máximo de 100 caractéres, favor verificar."))]
+        public string Autor { get; set; }
+
+        [Required]
+        public bool Disponivel { get; set; }
+
+        #region [Relacionamento]
+
+        public int IdCategoria { get; set; }
+
+        public virtual Categoria Categorias { get; set; }
+
+        #endregion
+    }
+}
