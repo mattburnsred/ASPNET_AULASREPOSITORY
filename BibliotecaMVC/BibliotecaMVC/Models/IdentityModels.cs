185e04c42db06c1d045be1e05c1c19ce990e7537
Matheus Tessaro <matbrnsred@gmail.com>
2017-07-04 17:51:38 -0300

    Início da implementação do CRUD de Livros / Alterações nas views de Categorias

--- BibliotecaMVC/BibliotecaMVC/Models/IdentityModels.cs
+++ BibliotecaMVC/BibliotecaMVC/Models/IdentityModels.cs
@@ -31,5 +31,7 @@ namespace BibliotecaMVC.Models
         }
 
         public System.Data.Entity.DbSet<BibliotecaMVC.Models.Categoria> Categorias { get; set; }
+
+        public System.Data.Entity.DbSet<BibliotecaMVC.Models.Livro> Livroes { get; set; }
     }
 }
